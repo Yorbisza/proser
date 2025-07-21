ac1f00bad88d04e4d7bca2ce590bb263415314ba
Yorbis Acevedo <yacevedo@inea.gob.ve>
2025-07-21 08:57:41 -0400

    Cambios

--- resources/views/modules/servidores/create.blade.php
+++ resources/views/modules/servidores/create.blade.php
@@ -1,68 +1,67 @@
-@extends('layouts/main');
+@extends('layouts/main')
+
 <div class="container mt-4">
     <div class="row">
         <div class="col">
-            <div class="card ">
+            <div class="card">
                 <div class="card-body">
-                    <form action="{{ route('serve.store') }}" method="POST" name="proser" id="proser"
-                        class="row g-3 needs-validation" novalidate>
+                    <form action="{{ route('servidores.store') }}" method="POST" name="proser" id="proser" class="row g-3 needs-validation" novalidate>
                         @csrf
                         @method('POST')
+
                         <div class="col-md-4">
                             <div class="form-group">
                                 <label for="nombre_servidores">Nombre Servidor:</label>
-                                <input type="text" class="form-control" id="nombre_servidores"
-                                    name="nombre_servidores" required>
-                                    <div class="invalid-feedback">
-                                        Por favor colocar nombre del servidor.
-                                      </div>
+                                <input type="text" class="form-control" id="nombre_servidores" name="nombre_servidores" required>
+                                <div class="invalid-feedback">
+                                    Por favor, coloca el nombre del servidor.
+                                </div>
                             </div>
                         </div>
+
                         <div class="col-md-4">
                             <div class="form-group">
                                 <label for="ip_servidores">IP Servidor:</label>
-                                <input type="text" class="form-control" id="ip_servidores" name="ip_servidores"
-                                    required>
-                                    <div class="invalid-feedback">
-                                        Por favor colocar IP del Servidor.
-                                      </div>
+                                <input type="text" class="form-control" id="ip_servidores" name="ip_servidores" required>
+                                <div class="invalid-feedback">
+                                    Por favor, coloca la IP del servidor.
+                                </div>
                             </div>
                         </div>
-                        <div class="col-md-4">
 
+                        <div class="col-md-4">
                             <div class="form-group">
                                 <label for="puerto">Puerto:</label>
                                 <input type="number" class="form-control" id="puerto" name="puerto" required>
                                 <div class="invalid-feedback">
-                                    Por favor colocar el puerto del servidor.
-                                  </div>
+                                    Por favor, coloca el puerto del servidor.
+                                </div>
                             </div>
                         </div>
-                        <div class="col-md-4">
 
+                        <div class="col-md-4">
                             <div class="form-group">
                                 <label for="nombre_usuario">Nombre usuario:</label>
-                                <input type="text" class="form-control" id="nombre_usuario" name="nombre_usuario"
-                                    required>
-                                    <div class="invalid-feedback">
-                                        Por favor colocar nombre del usuario.
-                                      </div>
+                                <input type="text" class="form-control" id="nombre_usuario" name="nombre_usuario" required>
+                                <div class="invalid-feedback">
+                                    Por favor, coloca el nombre del usuario.
+                                </div>
                             </div>
                         </div>
-                        <div class="col-xs-3 col-sm-12 col-md-4">
 
+                        <div class="col-md-4">
                             <div class="form-group">
                                 <label for="contrasenas">Contraseña:</label>
-                                <input type="text" class="form-control" id="contrasenas" name="password" required>
+                                <input type="password" class="form-control" id="contrasenas" name="password" required>
                                 <div class="invalid-feedback">
-                                    Por favor colocar la contraseña.
-                                  </div>
+                                    Por favor, coloca la contraseña.
+                                </div>
                             </div>
                         </div>
-                        <div class="col-xs-3 col-sm-12 col-md-4">
 
+                        <div class="col-md-4">
                             <div class="form-group">
-                                <label for="ambiente_id">Ambiente: </label>
+                                <label for="ambiente_id">Ambiente:</label>
                                 <select name="ambiente_id" id="ambiente_id" class="form-select" required>
                                     <option value="">Seleccione</option>
                                     @foreach ($ambientes as $a)
@@ -70,13 +69,14 @@
                                     @endforeach
                                 </select>
                                 <div class="invalid-feedback">
-                                    Debe selecionar un ambiente.
-                                  </div>
+                                    Debe seleccionar un ambiente.
+                                </div>
                             </div>
                         </div>
-                        <div class="col-xs-3 col-sm-12 col-md-4">
+
+                        <div class="col-md-4">
                             <div class="form-group">
-                                <label for="status_id">Status: </label>
+                                <label for="status_id">Status:</label>
                                 <select name="status_id" id="status_id" class="form-select" required>
                                     <option value="">Seleccione</option>
                                     @foreach ($status as $st)
@@ -84,40 +84,41 @@
                                     @endforeach
                                 </select>
                                 <div class="invalid-feedback">
-                                    Debe selecionar un status
-                                  </div>
+                                    Debe seleccionar un status.
+                                </div>
                             </div>
                         </div>
-                        <div class="col-12">
-                        <button type="submit" class="btn btn-primary mt-3">Enviar</button>
 
-                        <a href="{{ route('servidores.index') }}" class="btn btn-secondary mt-3">Cancelar</a>
-                    </div>
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary mt-3">Enviar</button>
+                            <a href="{{ route('servidores.index') }}" class="btn btn-secondary mt-3">Cancelar</a>
+                        </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 <script>
-    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    // JavaScript para manejar la validación del formulario
     (function() {
-        'use strict'
+        'use strict';
 
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.querySelectorAll('.needs-validation')
+        // Obtener todos los formularios que queremos aplicar estilos de validación
+        var forms = document.querySelectorAll('.needs-validation');
 
-        // Loop over them and prevent submission
+        // Recorrerlos y prevenir el envío si hay campos inválidos
         Array.prototype.slice.call(forms)
             .forEach(function(form) {
                 form.addEventListener('submit', function(event) {
                     if (!form.checkValidity()) {
-                        event.preventDefault()
-                        event.stopPropagation()
+                        event.preventDefault(); // Evita el envío del formulario
+                        event.stopPropagation(); // Detiene la propagación del evento
                     }
 
-                    form.classList.add('was-validated')
-                }, false)
-            })
-    })()
+                    form.classList.add('was-validated'); // Añadir clase para mostrar estilos de Bootstrap
+                }, false);
+            });
+    })();
 </script>
